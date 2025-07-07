3ebad62657b4659f8d7c972a19c21f2dfd427fe4
Rogersovich <53912205+rogersovich@users.noreply.github.com>
2025-07-07 23:19:06 +0700

    Add Pokemon type utility function and integrate into base stats component
    - Introduced `apalah` function in `pokemon-size.ts` to calculate weak points for Pokemon types.
    - Updated `PokemonBaseStat` component to utilize the new `apalah` function and log the resulting type information.
    - Enhanced type management for better gameplay mechanics and interactions.

--- src/lib/pokemon-size.ts
+++ src/lib/pokemon-size.ts
@@ -41,3 +41,109 @@ export function hectogramsToPounds(hectograms: number): string {
   const pounds = hectograms * 0.220462;
   return pounds.toFixed(1);
 }
+
+export function apalah() {
+  const list_types = [
+    {
+      name: 'grass',
+      weak_point: 0
+    },
+    {
+      name: 'bug',
+      weak_point: 0
+    },
+  ]
+
+  const pokemon_types = [
+    {
+      name: 'grass',
+      ddf: [
+        {
+          name: 'bug'
+        },
+        {
+          name: 'flying'
+        },
+        {
+          name: 'poison'
+        },
+        {
+          name: 'fire'
+        },
+        {
+          name: 'ice'
+        },
+      ],
+      hdf: [
+        {
+          name: 'ground'
+        },
+        {
+          name: 'water'
+        },
+        {
+          name: 'grass'
+        },
+        {
+          name: 'electric'
+        },
+      ]
+    },
+    {
+      name: 'poison',
+      ddf: [
+        {
+          name: 'ground'
+        },
+        {
+          name: 'psychic'
+        },
+      ],
+      hdf: [
+        {
+          name: 'fighting'
+        },
+        {
+          name: 'poison'
+        },
+        {
+          name: 'bug'
+        },
+        {
+          name: 'grass'
+        },
+        {
+          name: 'fairy'
+        },
+      ]
+    },
+  ]
+
+  list_types.forEach((type, i_type) => {
+    if(type.name === 'grass') {
+      let weakPoint = 1
+      pokemon_types.forEach(p_type => {
+        const ddf_type = p_type.ddf.filter(hdf => hdf.name == type.name).length
+        const hdf_type = p_type.hdf.filter(hdf => hdf.name == type.name).length
+
+        let ddf_point = 0
+        let hdf_point = 0
+
+        if(ddf_type > 0){
+          ddf_point = 2
+        }
+
+        if (hdf_type > 0) {
+          hdf_point = 0.5
+        }
+        const weakCalc = ddf_point + hdf_point
+
+        weakPoint = weakPoint * weakCalc
+      });
+
+      list_types[i_type].weak_point = weakPoint
+    }
+  });
+
+  return list_types
+}
